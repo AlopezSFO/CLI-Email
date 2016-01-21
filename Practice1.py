2563c34bf8c5b1c3bfc7036667f6afeb4f1e85fe
AlopezSFO <alopezsfo@gmail.com>
2016-01-21 12:46:08 -0800

    hit enter for to field

--- Practice1.py
+++ Practice1.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 print('Lets go!')
@@ -35,7 +36,8 @@ composeElem.click()
 #click recipient and input
 toElem = browser.find_element_by_id('to-field')
 toElem.send_keys(userSend)
-WebElement.sendKeys(Keys.RETURN);
+toElem.send_keys(Keys.ENTER);
+
 
 subElem = browser.find_element_by_id('subject-field')
 subElem.send_keys(userSubject)
